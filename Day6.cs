2ab8a4d497bc19a95fd1af1243c4f090f3fb6854
Ali Huseynli <99591534+HuseinliAli@users.noreply.github.com>
2022-06-29 20:37:32 +0400

    Day6I Lets Preview

---
+++ Day6.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+class Solution
+{
+    static void Main(String[] args)
+    {
+        /* Enter your code here. Read input from STDIN. Print output to STDOUT. Your class should be named Solution */
+        int StringNumber=int.Parse(Console.ReadLine());
+        for(int i = 0; i<StringNumber; i++)
+        {
+            string formatting = Console.ReadLine();
+            StringSplit(formatting);
+        }
+        Console.ReadKey();
+    }
+    static void StringSplit(string formatting)
+    {
+        string odd = "";
+        string even = "";
+        for (int i = 0; i<formatting.Length; i++)
+        {
+            if (i%2==0)
+            {
+                odd+=formatting[i];
+            }
+        }
+        for (int i = 0; i<formatting.Length; i++)
+        {
+            if (i%2!=0)
+            {
+                even+=formatting[i];
+            }
+        }
+        Console.WriteLine(odd+" "+even);
+        
+    }
+}
