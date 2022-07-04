083a6fcbce12850d141a8e9c537d9901480840cf
Ali Huseynli <99591534+HuseinliAli@users.noreply.github.com>
2022-07-04 07:08:00 +0400

    Day10: Binary Numbers

---
+++ Day10.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Day10
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            int counterOfOne=0;
+            int counterOfTwo=0;
+            while(n!=0)
+            {
+                if(n%2==1)
+                {
+                    counterOfOne++;
+                    if(counterOfOne>counterOfTwo)
+                    {
+                        counterOfTwo=counterOfOne;
+                    }
+                }
+                else
+                {
+                    counterOfOne=0;
+                }
+                n/=2;
+            }
+            Console.WriteLine(counterOfTwo);
+            Console.ReadKey();
+        }
+    }
+}
