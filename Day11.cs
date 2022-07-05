3ca3cd0c4f0c962f2ef6ea353d65dea1bbd0274a
Ali Huseynli <99591534+HuseinliAli@users.noreply.github.com>
2022-07-05 12:33:05 +0400

    Day11: 2D Arrays

---
+++ Day11.cs
@@ -0,0 +1,41 @@
+﻿using System.CodeDom.Compiler;
+using System.Collections.Generic;
+using System.Collections;
+using System.ComponentModel;
+using System.Diagnostics.CodeAnalysis;
+using System.Globalization;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Runtime.Serialization;
+using System.Text.RegularExpressions;
+using System.Text;
+using System;
+
+
+
+class Solution
+{
+    public static void Main(string[] args)
+    {
+
+        List<List<int>> arr = new List<List<int>>();
+
+        for (int i = 0; i < 6; i++)
+        {
+            arr.Add(Console.ReadLine().TrimEnd().Split(' ').ToList().Select(arrTemp => Convert.ToInt32(arrTemp)).ToList());
+        }
+        int sum = 0;
+        int max = int.MinValue;
+        for(int i=0;i<4;i++)
+        {
+            for (int j = 0; j<4; j++)
+            {
+                sum=arr[i][j]+arr[i+1][j]+arr[i+2][j]+arr[i+1][j+1]+arr[i+2][j]+arr[i+2][j+1]+arr[i+2][j+2];
+            }
+            max=Math.Max(max,sum);
+        }
+        Console.WriteLine(max);
+        Console.ReadKey();
+    }
+}
