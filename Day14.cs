c43710288b0ca52ad8b00c7a27b7f8a3d08a95bb
Ali Huseynli <99591534+HuseinliAli@users.noreply.github.com>
2022-07-07 10:51:28 +0400

    Day14

---
+++ Day14.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Linq;
+
+class Difference
+{
+    private int[] elements;
+    public int maximumDifference;
+
+    // Add your code here
+    public Difference(int[] arr)
+    {
+        this.elements=arr;
+    }
+    public void computeDifference()
+    {
+        int max = int.MinValue;
+        int min = int.MaxValue;
+        for (int i = 0; i<elements.Length; i++)
+        {
+            if (elements[i]>max)
+            {
+                max=elements[i];
+            }
+        }
+        for (int i = 0; i<elements.Length; i++)
+        {
+            if (elements[i]<min)
+            {
+                min=elements[i];
+            }
+        }
+        maximumDifference=max-min;
+    }
+} // End of Difference Class
+
+class Solution
+{
+    static void Main(string[] args)
+    {
+        Convert.ToInt32(Console.ReadLine());
+
+        int[] a = Console.ReadLine().Split(' ').Select(x => Convert.ToInt32(x)).ToArray();
+
+        Difference d = new Difference(a);
+
+        d.computeDifference();
+
+        Console.Write(d.maximumDifference);
+        Console.ReadKey();
+    }
+}
