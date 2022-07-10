8b52e1200d33719f63670f9a85446de91f815c33
Ali Huseynli <99591534+HuseinliAli@users.noreply.github.com>
2022-07-10 10:39:12 +0400

    Day16: Exception

---
+++ Day16.cs
@@ -0,0 +1,34 @@
+﻿using System.CodeDom.Compiler;
+using System.Collections.Generic;
+using System.Collections;
+using System.ComponentModel;
+using System.Diagnostics.CodeAnalysis;
+using System.Globalization;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Runtime.Serialization;
+using System.Text.RegularExpressions;
+using System.Text;
+using System;
+
+
+
+class Solution
+{
+    public static void Main(string[] args)
+    {
+        int S;
+        try
+        {
+            S=int.Parse(Console.ReadLine());
+            Console.WriteLine(S);
+        }
+        catch (FormatException) 
+        {
+            Console.WriteLine("Bad String");
+        }
+        Console.ReadKey();
+        
+    }
+}
