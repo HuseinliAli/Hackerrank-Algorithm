a7044a1b008f7b18dc4e8608d018dfed05d1c8d6
Ali Huseynli <99591534+HuseinliAli@users.noreply.github.com>
2022-07-20 04:52:16 +0400

    Day25: Running Time and Complexity

---
+++ Day25.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+class Solution
+{
+    static void Main(String[] args)
+    {
+        /* Enter your code here. Read input from STDIN. Print output to STDOUT. Your class should be named Solution */
+        Console.WriteLine("Say Daxil Edin:");
+        int n = int.Parse(Console.ReadLine());
+        int[] array = new int[n];
+        Console.WriteLine("Elementleri Daxil Edin");
+        for (int i = 0; i<n; i++)
+        {
+            int elementOfArray = int.Parse(Console.ReadLine());
+            array[i] = elementOfArray;
+        }
+        int primaryTester = 0;
+        for (int i = 0; i<n; i++)
+        {
+            for (int j = 2; j<Math.Sqrt(array[i]); j++)
+            {
+                if (array[i]%j==0)
+                {
+                    primaryTester++;
+                }
+            }
+            if(primaryTester>0)
+            {
+                Console.WriteLine("Not prime");
+                primaryTester=0;
+            }
+            else
+            {
+                Console.WriteLine("Prime");
+            }
+        }
+        Console.ReadKey();
+    }
+}
